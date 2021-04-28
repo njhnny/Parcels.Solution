1ef8ff5022f6f1c77d63f42ef373f83f86a2ba3e
Nick Hennessy <njhnny@gmail.com>
2021-04-27 20:41:56 -0700

    update package
    Co-authored-by: Theron Packus <tlpackus@gmail.com>

--- Parcels/Models/Package.cs
+++ Parcels/Models/Package.cs
@@ -1,1 +1,30 @@
 using System.Collections.Generic;
+
+namespace Parcels.Models
+{
+  public class Package
+  {
+    public string Length { get; set; }
+    public string Height { get; set; }
+    public string Width { get; set; }
+    public string Weight { get; set; }
+    private static List<Package> _instances = new List<Package> {};
+
+    public Package (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Package> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+  }
+}
